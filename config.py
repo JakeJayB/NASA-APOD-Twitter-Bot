d5047f349f43c7199d1fbe53711f0b8731c576a1
JakeJayB <JacobJBejarano@gmail.com>
2026-03-20 00:50:21 -0700

    Removed TwitterAPI for project change

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    X_BEARER_TOKEN: str
-    X_API_KEY: str
-    X_API_SECRET: str
-    X_ACCESS_TOKEN: str
-    X_ACCESS_SECRET: str
-    X_CLIENT_ID: str
-    X_CLIENT_SECRET: str
     NASA_APOD_KEY: str
 
 
